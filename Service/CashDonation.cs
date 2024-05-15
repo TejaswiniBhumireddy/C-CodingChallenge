2ec6b9bcede114fc7c707653714e020c4870726a
TejaswiniBhumireddy <106535392+TejaswiniBhumireddy@users.noreply.github.com>
2024-05-15 13:52:26 +0530

    Add files via upload

---
+++ Service/CashDonation.cs
@@ -0,0 +1,33 @@
+﻿using PetPals.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetPals.Service
+{
+    public class CashDonation : Donation
+    {
+        public DateTime DonationDate { get; set; }
+        private const decimal MinimumDonationAmount = 10000;
+
+
+        public CashDonation(string donorName, decimal amount, DateTime donationDate)
+            : base(donorName, amount)
+        {
+            if (amount < MinimumDonationAmount)
+            {
+                throw new InsufficientFundsException($"Donation amount should be at least {MinimumDonationAmount}");
+            }
+            DonorName = donorName;
+            Amount = amount;
+        }
+
+        public override void RecordDonation()
+        {
+            Console.WriteLine($"Cash donation recorded: Donor: {DonorName}, Amount: {Amount}, Date: {DonationDate}");
+        }
+    }
+
+}
---
+++ Service/ItemDonation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetPals.Service
+{
+    public class ItemDonation : Donation
+    {
+        public string ItemType { get; set; }
+
+        public ItemDonation(string donorName, decimal amount, string itemType)
+            : base(donorName, amount)
+        {
+            ItemType = itemType;
+        }
+
+        public override void RecordDonation()
+        {
+            Console.WriteLine($"Item donation recorded: Donor: {DonorName}, Amount: {Amount}, ItemType: {ItemType}");
+        }
+    }
+}
