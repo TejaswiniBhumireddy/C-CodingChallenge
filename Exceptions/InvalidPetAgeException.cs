483797994800e744738c37d0fd8148231d42140c
TejaswiniBhumireddy <106535392+TejaswiniBhumireddy@users.noreply.github.com>
2024-05-15 13:50:53 +0530

    Add files via upload

---
+++ Exceptions/InvalidPetAgeException.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace PetPals.Exceptions
+{
+    internal class InvalidPetAgeException : Exception
+    {
+        public InvalidPetAgeException()
+        {
+        }
+
+        public InvalidPetAgeException(string? message) : base(message)
+        {
+        }
+
+    }
+}
