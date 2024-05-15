91ea6e7572d1efeb50cd0a201386146932af003e
TejaswiniBhumireddy <106535392+TejaswiniBhumireddy@users.noreply.github.com>
2024-05-15 13:35:24 +0530

    Add files via upload

---
+++ Cat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetPals.Model
+{
+    internal class Cat : Pet
+    {
+        public string CatColor { get; set; }
+
+        public Cat(string name, int age, string breed, string catColor)
+            : base(name, age, breed)
+        {
+            CatColor = catColor;
+        }
+
+        public override string ToString()
+        {
+            return $"Cat(Name: {Name}, Age: {Age}, Breed: {Breed}, CatColor: {CatColor})";
+        }
+    }
+}
---
+++ PetShelter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetPals.Model
+{
+    internal class PetShelter
+    {
+        private List<Pet> availablePets = new List<Pet>();
+
+        public void AddPet(Pet pet)
+        {
+            availablePets.Add(pet);
+        }
+
+        public void RemovePet(Pet pet)
+        {
+            availablePets.Remove(pet);
+        }
+        public void ListAvailablePets()
+        {
+            foreach (var pet in availablePets)
+            {
+                try
+                {
+                    Console.WriteLine($"Name: {pet.Name}, Age: {pet.Age}, Breed: {pet.Breed}");
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("One or more properties of the pet are missing.");
+                }
+            }
+        }
+    }
+}
