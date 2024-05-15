d3a28148fffd380b9a2b2e6f6a87e9ffba4b9113
TejaswiniBhumireddy <106535392+TejaswiniBhumireddy@users.noreply.github.com>
2024-05-15 13:42:00 +0530

    Add files via upload

---
+++ InsufficientFundsException.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace PetPals.Exceptions
+{
+    internal class InsufficientFundsException : Exception
+    {
+        public InsufficientFundsException()
+        {
+        }
+
+        public InsufficientFundsException(string? message) : base(message)
+        {
+        }
+
+    }
+}
